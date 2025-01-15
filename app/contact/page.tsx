33f1a2d8db1a659fa11fcbed17a8bcf70ff1c41e
Samuel Berrus <samuelberrus@gmail.com>
2025-01-15 22:40:52 +0100

    Añade cambios en el diseño

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,14 +4,11 @@ import gfont from '../fonts/custom1'
 const page = () => {
     return (
         <div className="max-w-screen-lg mx-auto p-5 pt-24">
-            <div className="grid grid-cols-1 md:grid-cols-12 border">
+            <div className="grid grid-cols-1 md:grid-cols-12 border border-yellow-950">
                 <div className="bg-dark md:col-span-4 p-10 text-white">
-                    <p className="mt-4 text-sm leading-7 font-regular uppercase">
-                        Contact
-                    </p>
                     <h3 className={`${gfont.className} text-3xl sm:text-4xl leading-normal font-extrabold tracking-tight`}>
-                        ¿Listo para dejar tu
-                        <span className="text-primary"> marca en el mapa?</span>
+                        ¿Listo para dejar tu{" "}
+                        <span className="text-primary">marca</span> {" "}en el mapa?
                     </h3>
                     <p className="mt-4 leading-7 text-gray-200">
                         Estamos listos para escuchar tus ideas y transformarlas en arte que llevarás contigo para siempre. Escríbenos, hablemos de tu diseño y hagamos que esta historia quede grabada en tu piel. ¡Contáctanos hoy!
@@ -83,59 +80,50 @@ const page = () => {
                 <form className="md:col-span-8 p-10">
                     <div className="flex flex-wrap -mx-3 mb-6">
                         <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                            <label className="block uppercase tracking-wide text-yellow-400 text-xs font-bold mb-2"
                                 htmlFor="grid-first-name">
-                                First Name
+                                Nombre
                             </label>
                             <input
-                                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                                className="appearance-none block w-full bg-gray-200 text-yellow-400 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                                 id="grid-first-name" type="text" placeholder="Jane" />
-                            <p className="text-red-500 text-xs italic">Please fill out this field.</p>
                         </div>
                         <div className="w-full md:w-1/2 px-3">
-                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                            <label className="block uppercase tracking-wide text-yellow-400 text-xs font-bold mb-2"
                                 htmlFor="grid-last-name">
-                                Last Name
+                                Apellido
                             </label>
                             <input
-                                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                className="appearance-none block w-full bg-gray-200 text-yellow-400 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                                 id="grid-last-name" type="text" placeholder="Doe" />
                         </div>
                     </div>
                     <div className="flex flex-wrap -mx-3 mb-6">
                         <div className="w-full px-3">
-                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                            <label className="block uppercase tracking-wide text-yellow-400 text-xs font-bold mb-2"
                                 htmlFor="grid-password">
-                                Email Address
+                                Correo electrónico
                             </label>
                             <input
-                                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                className="appearance-none block w-full bg-gray-200 text-yellow-400 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                                 id="grid-email" type="email" placeholder="********@*****.**" />
                         </div>
                     </div>
 
                     <div className="flex flex-wrap -mx-3 mb-6">
                         <div className="w-full px-3">
-                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                            <label className="block uppercase tracking-wide text-yellow-400 text-xs font-bold mb-2"
                                 htmlFor="grid-password">
-                                Your Message
+                                Mensaje
                             </label>
                             <textarea rows={10}
-                                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"></textarea>
+                                className="appearance-none block w-full bg-gray-200 text-yellow-400 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"></textarea>
                         </div>
-                        <div className="flex justify-between w-full px-3">
-                            <div className="md:flex md:items-center">
-                                <label className="block text-gray-500 font-bold">
-                                    <input className="mr-2 leading-tight" type="checkbox" />
-                                    <span className="text-sm">
-                                        Send me your newsletter!
-                                    </span>
-                                </label>
-                            </div>
+                        <div className="flex justify-end w-full px-3">
                             <button
-                                className="shadow bg-indigo-600 hover:bg-indigo-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-6 rounded"
+                                className="shadow bg-yellow-400 hover:bg-indigo-400 focus:shadow-outline focus:outline-none text-yellow-950 font-bold py-2 px-6 rounded"
                                 type="submit">
-                                Send Message
+                                Enviar mensaje
                             </button>
                         </div>
 
